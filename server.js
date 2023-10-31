a62711d6e0f21c6bdbb0573431ce8ef614d6446a
clvalada <clvalada@gmail.com>
2023-10-31 10:56:33 -0400

    updating routes

--- server.js
+++ server.js
@@ -90,12 +90,15 @@ app.delete('/api/notes/:id', (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
-  
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+  
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
