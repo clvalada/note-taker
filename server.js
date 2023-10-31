b4c9de2ed0bf90e7fc6dd8e559ae59e4626b642c
clvalada <clvalada@gmail.com>
2023-10-31 10:14:22 -0400

    troubleshooting server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.delete('/api/notes/:id', (req, res) => {
   
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
