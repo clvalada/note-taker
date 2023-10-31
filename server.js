abf4d54916038a18b5c8011bf7e6c5dad6677658
clvalada <clvalada@gmail.com>
2023-10-31 10:03:38 -0400

    server.js code

--- server.js
+++ server.js
@@ -7,17 +7,89 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+
+// Read notes from JSON File and send as a response
 app.get('/api/notes', (req, res) => {
-  // Implement logic to read notes from the JSON file and send them as a response.
-});
+    try {
+      // Read the contents of the 'notes.json' file
+      const notesData = fs.readFileSync('notes.json', 'utf8');
+  
+      // Parse the JSON data into an array
+      const notes = JSON.parse(notesData);
+  
+      // Send the notes as a JSON response
+      res.json(notes);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
+ //Add new note to the JSON file 
 app.post('/api/notes', (req, res) => {
-  // Implement logic to add a new note to the JSON file.
-});
+    try {
+        // Read the contents of the 'notes.json' file
+        const notesData = fs.readFileSync('notes.json', 'utf8');
 
+        // Parse the JSON data into an array
+        const notes = JSON.parse(notesData);
+
+        // Create a new note object based on the request body
+        const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        };
+  
+      // Assign a unique ID to the new note (you can use a timestamp or a unique identifier library)
+      newNote.id = // Generate a unique ID;
+  
+      // Add the new note to the array of notes
+      notes.push(newNote);
+  
+      // Write the updated notes array back to the 'notes.json' file
+      fs.writeFileSync('notes.json', JSON.stringify(notes, null, 2));
+  
+      // Send a success response
+      res.json(newNote);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+//delete a note from the JSON file.  
 app.delete('/api/notes/:id', (req, res) => {
-  // Implement logic to delete a note from the JSON file.
-});
+    try {
+      const noteId = req.params.id;
+  
+      // Read the contents of the 'notes.json' file
+      const notesData = fs.readFileSync('notes.json', 'utf8');
+  
+      // Parse the JSON data into an array
+      const notes = JSON.parse(notesData);
+  
+      // Find the index of the note to be deleted
+      const noteIndex = notes.findIndex((note) => note.id === noteId);
+  
+      if (noteIndex !== -1) {
+        // Remove the note from the array
+        notes.splice(noteIndex, 1);
+  
+        // Write the updated notes array back to the 'notes.json' file
+        fs.writeFileSync('notes.json', JSON.stringify(notes, null, 2));
+  
+        // Send a success response
+        res.json({ message: 'Note deleted' });
+      } else {
+        // Send a not found response if the note with the specified ID doesn't exist
+        res.status(404).json({ error: 'Note not found' });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
 
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
